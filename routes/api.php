40dec29af98ed828bba20583869402a574a0c1d1
aditiyapratama <aditiyapratama2014@gmail.com>
2026-07-25 15:02:35 +0700

    Feat : Add AuditCategoryController

--- routes/api.php
+++ routes/api.php
@@ -2,5 +2,16 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\AuditCategoryController;
 
 Route::post('/login', [AuthController::class, 'login']);
+
+// Audit Category Api
+
+Route::prefix('audit/categories')->group(function () {
+    Route::get('/', [AuditCategoryController::class, 'index']);
+    Route::get('/{id}', [AuditCategoryController::class, 'show']);
+    Route::post('/', [AuditCategoryController::class, 'store']);
+    Route::put('/{id}', [AuditCategoryController::class, 'update']);
+    Route::delete('/{id}', [AuditCategoryController::class, 'destroy']);
+});
